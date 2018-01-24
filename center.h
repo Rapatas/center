7196346351a2471ffa8540690ca9002960e7552a
Andreas Rapatas <15367779+AndreasRapatas@users.noreply.github.com>
2018-01-24 12:37:30 +0200

    Made function inline

--- center.h
+++ center.h
@@ -3,10 +3,9 @@
 
 #include <string>
 
-std::string center(const std::string &text, unsigned width = 80) {
+std::string inline center(const std::string &text, unsigned width = 80) {
 
-	// If the string is longer than the specified width
-	// don't do anything.
+	// If the string is longer than the specified width don't do anything.
 	if (text.size() >= width) { return text; }
 
 	unsigned padding_width = (width / 2) - (text.size() / 2);
