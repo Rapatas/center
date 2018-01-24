ade1077c9589ef493e9de92e234972029b73f9a6
Andreas Rapatas <15367779+AndreasRapatas@users.noreply.github.com>
2018-01-24 14:18:17 +0200

    Updated tests

--- test.cpp
+++ test.cpp
@@ -20,8 +20,11 @@ int main() {
 	text = "0123456789+";
 	assert(center(text, 10) == text);
 
-	// Centering a string of even length on a line of even width should add
-	// an even number of spaces
+	// Centering a string of length 4 on a line of width 8 should add 2 spaces.
 	text = "1234";
 	assert(center(text, 8).size() == text.size() + 2);
+
+	// Centering a string of length 5 on a line of width 7 should add 1 spaces.
+	text = "12345";
+	assert(center(text, 7).size() == text.size() + 1);
 }
