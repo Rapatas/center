56c85f0dbc530f17d6b6390902efab563284a5fa
Andreas Rapatas <15367779+AndreasRapatas@users.noreply.github.com>
2018-01-24 18:47:40 +0200

    Added detection of terminal width in linux

--- center.h
+++ center.h
@@ -3,7 +3,27 @@
 
 #include <string>
 
-std::string inline center(const std::string &text, unsigned width = 80) {
+#ifdef __linux__
+#include <sys/ioctl.h>
+#include <unistd.h>
+#elif _WIN32
+#endif
+
+// This function will be called if the user does not provide a line width
+unsigned get_terminal_width() {
+#ifdef __linux__
+	struct winsize size;
+	ioctl(STDOUT_FILENO,TIOCGWINSZ,&size);
+	return size.ws_col;
+#elif _WIN32
+	return 80;
+#endif
+}
+
+std::string inline center(
+	const std::string &text,
+	unsigned width = get_terminal_width()
+) {
 
 	// If the string is longer than the specified width don't do anything.
 	if (text.size() >= width) { return text; }
