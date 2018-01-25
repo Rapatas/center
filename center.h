d7b9ff92880718e94bfd86a6ad5e233b3ae10281
Androu <15367779+AndreasRapatas@users.noreply.github.com>
2018-01-25 11:03:28 +0200

    Added back the get_terminal_width function

--- center.h
+++ center.h
@@ -10,25 +10,26 @@
 #include <windows.h>
 #endif
 
+unsigned get_terminal_width() {
+#ifdef __linux__
+	struct winsize size;
+	ioctl(STDOUT_FILENO,TIOCGWINSZ,&size);
+	return size.ws_col;
+#elif _WIN32
+	CONSOLE_SCREEN_BUFFER_INFO csbi;
+	GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi);
+	return csbi.srWindow.Right - csbi.srWindow.Left + 1;
+#else
+	// If there is no way to find the terminal width, set it to 80
+	return 80;
+#endif
+}
+
 std::string inline center(
 	const std::string &text,
-	// This lambda will be called if the user does not provide a line width
-	unsigned width = []() -> unsigned {
-#ifdef __linux__
-		struct winsize size;
-		ioctl(STDOUT_FILENO,TIOCGWINSZ,&size);
-		return size.ws_col;
-#elif _WIN32
-		CONSOLE_SCREEN_BUFFER_INFO csbi;
-		GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi);
-		return csbi.srWindow.Right - csbi.srWindow.Left + 1;
-#else
-		return 80;
-#endif
-	} ()
+	unsigned width = get_terminal_width()
 ) {
-
-	// If the string is longer than the specified width don't do anything.
+	// If the string is longer than the specified width, don't do anything.
 	if (text.size() >= width) { return text; }
 
 	unsigned padding_width = (width / 2) - (text.size() / 2);
