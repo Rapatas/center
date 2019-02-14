9418a7ef31ac746eaa1bd91254d04f09031a2c7f
Andy Rapatas <15367779+Rapatas@users.noreply.github.com>
2019-02-14 14:03:27 +0200

    Split implementation in source file

--- include/center.h
+++ include/center.h
@@ -3,38 +3,11 @@
 
 #include <string>
 
-#ifdef __linux__
-#include <sys/ioctl.h>
-#include <unistd.h>
-#elif _WIN32
-#include <windows.h>
-#endif
+unsigned get_terminal_width();
 
-unsigned get_terminal_width() {
-#ifdef __linux__
-	struct winsize size;
-	ioctl(STDOUT_FILENO,TIOCGWINSZ,&size);
-	return size.ws_col;
-#elif _WIN32
-	CONSOLE_SCREEN_BUFFER_INFO csbi;
-	GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi);
-	return csbi.srWindow.Right - csbi.srWindow.Left + 1;
-#else
-	// If there is no way to find the terminal width, set it to 80
-	return 80;
-#endif
-}
-
-std::string inline center(
+std::string center(
 	const std::string &text,
 	unsigned width = get_terminal_width()
-) {
-	// If the string is longer than the specified width, don't do anything.
-	if (text.size() >= width) { return text; }
-
-	unsigned padding_width = (width / 2) - (text.size() / 2);
-
-	return std::string(padding_width, ' ') + text;
-}
+);
 
 #endif // CENTER_H
---
+++ src/center.cpp
@@ -0,0 +1,45 @@
+#include "../include/center.h"
+
+#ifdef __linux__
+#include <sys/ioctl.h>
+#include <unistd.h>
+#elif _WIN32
+#include <windows.h>
+#endif
+
+unsigned get_terminal_width() {
+
+#ifdef __linux__
+
+	struct winsize size;
+	ioctl(STDOUT_FILENO,TIOCGWINSZ,&size);
+	return size.ws_col;
+
+#elif _WIN32
+
+	CONSOLE_SCREEN_BUFFER_INFO csbi;
+	GetConsoleScreenBufferInfo(
+		GetStdHandle(STD_OUTPUT_HANDLE),
+		&csbi
+	);
+	return csbi.srWindow.Right - csbi.srWindow.Left + 1;
+
+#else
+
+	// If there is no way to find the terminal width, set it to 80
+	return 80;
+
+#endif
+}
+
+std::string center(
+	const std::string &text,
+	unsigned width
+) {
+	// If the string is longer than the specified width, don't do anything.
+	if (text.size() >= width) { return text; }
+
+	unsigned padding_width = (width / 2) - (text.size() / 2);
+
+	return std::string(padding_width, ' ') + text;
+}
